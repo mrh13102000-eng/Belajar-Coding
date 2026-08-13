22f99a2fb44dcc5ec2f136f2ba7223d6fe277c80
Ridho <mrh13102000@gmail.com>
2026-08-13 10:10:09 +0700

    Tambah module 29-1

---
+++ module29-1.js
@@ -0,0 +1,9 @@
+var nama = "Ridho";
+const makanan = "Bebek Goreng";
+const minuman = "Es Kelapa Muda";
+
+function sapa(){
+    console.log("Halo, nama saya " + nama + ". Saya suka makan " + makanan + " dan minum " + minuman);
+}
+
+sapa();
